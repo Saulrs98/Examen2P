d3ccca783d3a4963a53dd742d1979c39e616ee83
Saulrs98 <A01351663@itesm.mx>
2021-10-20 10:34:21 -0500

    funciones botones

---
+++ Zombi/agregar-nuevo-zombi.php
@@ -0,0 +1,8 @@
+<?php
+require_once("model.php");
+include("controlador-insertar-zombi.php");
+include("header.html");
+include("nav.html");
+include("agregar-nuevo-zombi.html");
+include("footer.html");
+?>
---
+++ Zombi/consultas.php
@@ -0,0 +1,9 @@
+<?php
+	$titulo="Consultas Dinamicas";
+	require_once("model.php");
+	include("header.html");
+	include("nav.html");
+	include("formconsultas.html");
+	include("despliegue-consultas.php");
+	include("footer.html");
+?>
